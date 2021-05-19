52a7030b2ff50e1f3caf89541764f6b31be09cd4
Salman Hanif <salmanhanif133@gmail.com>
2021-05-20 04:36:06 +0500

    aaaa

--- src/controllers/activity.controller.js
+++ src/controllers/activity.controller.js
@@ -71,7 +71,7 @@ const getActivitiesByOneDay = catchAsync(async (req, res) => {
     return ObjectId(value);
   });
   if (deviceArray.length < 1) {
-    aggregate.match({ userId: req.user._id });
+    aggregate.match({ userId: req.user._id, startDate: { $gt: new Date(today) } });
   } else {
     aggregate.match({
       userId: req.user._id,
